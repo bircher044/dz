e747818fdceee82e91ffa97098c4f5fc07ac6421
bircher044 <alexeybereza12345@gmail.com>
2022-02-22 15:07:10 +0200

    royalty change

--- contracts/Royalty.sol
+++ contracts/Royalty.sol
@@ -2,41 +2,23 @@
 
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-abstract contract ERC2981 is IERC2981, ERC165Storage {
+abstract contract ERC2981 is IERC2981 {
 
-    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
-
-    uint8 royaltyPercentage;
-
-    address owner;
-
-    mapping(uint256 => address) receiver;
-
-    constructor(uint8 _royaltyPercentage) {
-        royaltyPercentage = _royaltyPercentage;
-        owner = address(this);
-
-        _registerInterface(_INTERFACE_ID_ERC2981);
+    struct mappedRoyalties {
+        address receiver;
+        uint256 percentage;
     }
 
-    modifier OnlyContractOwner(){
-      require(msg.sender == owner, "You are not owner of the contract.");
-    _;
-    }
+    mapping(uint256 => mappedRoyalties) royalty;
 
-    function setReceiver(uint256 _tokenId, address _address) OnlyContractOwner public {
-        receiver[_tokenId] = _address;
-    }
-
-    function setRoyaltyPercentage(uint8 _royaltyPercentage) OnlyContractOwner public {
-        royaltyPercentage = _royaltyPercentage;
-    }
+    function _setRoyalties(uint256 _tokenId, address _receiver, uint256 _percentage) internal {
+        royalty[_tokenId] = mappedRoyalties(_receiver, _percentage);
+   }
 
     function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view override(IERC2981) returns (address Receiver, uint256 royaltyAmount) {
-        Receiver = receiver[_tokenId];
-        royaltyAmount = (_salePrice * royaltyPercentage) / 100;
+        Receiver = royalty[_tokenId].receiver;
+        royaltyAmount = _salePrice * royalty[_tokenId].percentage / 100;
     }
 }
